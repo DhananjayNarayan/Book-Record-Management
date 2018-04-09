6207b6ef8f26c17e7081c23261080d0a8adbd194
Dhananjay Narayan <37698599+DhananjayNarayan@users.noreply.github.com>
2018-04-09 17:41:53 +0530

    All PHP Files Updated

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home Page</title>
+<link rel="stylesheet" href="./css/viewStyle.css" />
+</head>
+<body bgcolor="LightCyan ">
+<h1>Book Record Management</h1>
+<a href="insertForm.php"> Insert Book </a><br/>
+<a href="View.php"> View Book Records</a><br/>
+<a href="deleteForm.php"> Delete Book Records </a><br/>
+<a href="updateForm.php"> Update Book Records </a><br/>
+</body>
+</html>
