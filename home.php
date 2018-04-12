b49a5b26188b56ce80ed81778cb45e9ed8c9dbf7
Dhananjay Narayan <37698599+DhananjayNarayan@users.noreply.github.com>
2018-04-12 21:50:58 +0530

    Update home.php

--- home.php
+++ home.php
@@ -5,10 +5,11 @@
 <link rel="stylesheet" href="./css/viewStyle.css" />
 </head>
 <body bgcolor="LightCyan ">
-<h1>Book Record Management</h1>
-<a href="insertForm.php"> Insert Book </a><br/>
+<h1 style="text-align:center;" >Book Record Management</h1>
+<p style="text-align:center;" ><a href="insertForm.php"> Insert Book </a><br/>
 <a href="View.php"> View Book Records</a><br/>
 <a href="deleteForm.php"> Delete Book Records </a><br/>
 <a href="updateForm.php"> Update Book Records </a><br/>
+</p>
 </body>
 </html>
